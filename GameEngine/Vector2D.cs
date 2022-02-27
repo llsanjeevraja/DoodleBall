0b9e27f976b1e64a8d1e4db0062c278e94e11642
Mayank <Mayank@DESKTOP-T4CRJ0N>
2022-02-27 08:30:40 +0530

    Add project files.

---
+++ GameEngine/Vector2D.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.GameEngine
+{
+        public class Vector2D
+        {
+            double X { get; set; }
+            double Y { get; set; }
+
+            public Vector2D()
+            {
+                this.X = Zero().X;
+                this.Y = Zero().Y;
+            }
+            public Vector2D(double x, double y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+            static Vector2D Zero()
+            {
+                return new Vector2D(0, 0);
+            }
+        }
+
+}
